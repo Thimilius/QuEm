251a905b036b2fa695a38a23e75ea7a5888f8229
Thimilius <mail@thimilius.com>
2022-06-12 17:19:58 +0200

    Use vector for matrix elements

--- quem/matrix.hpp
+++ quem/matrix.hpp
@@ -12,48 +12,13 @@ namespace QuEm {
   class MatrixT {
   public:
     MatrixT(uint64_t rows, uint64_t columns) : m_rows(rows), m_columns(columns) {
-      m_elements = new T[rows * columns];
+      m_elements.resize(rows * columns);
     }
     
-    MatrixT(uint64_t rows, uint64_t columns, std::initializer_list<T> elements) : m_rows(rows), m_columns(columns) {
+    MatrixT(uint64_t rows, uint64_t columns, const std::vector<T> &elements) : m_elements(elements), m_rows(rows), m_columns(columns) {
       assert(elements.size() == rows * columns);
-
-      m_elements = new T[rows * columns];
-      uint64_t i = 0;
-      for (auto element : elements) {
-        m_elements[i] = element;
-        i++;
-      }
-    }
-
-    MatrixT(const MatrixT &other) : m_rows(other.m_rows), m_columns(other.m_columns) {
-      m_elements = new T[m_rows * m_columns];
-      for (uint64_t i = 0; i < m_rows * m_columns; i++) {
-        m_elements[i] = other.m_elements[i];
-      }
-    }
-    
-    ~MatrixT() {
-      delete[] m_elements;
     }
   public:
-    MatrixT &operator=(const MatrixT &other) {
-      if (this == &other) {
-        return *this;
-      }
-
-      m_rows = other.m_rows;
-      m_columns = other.m_columns;
-      
-      delete[] m_elements;
-      m_elements = new T[m_rows * m_columns];
-      for (uint64_t i = 0; i < m_rows * m_columns; i++) {
-        m_elements[i] = other.m_elements[i];
-      }
-      
-      return *this;
-    }
-
     T &operator()(uint64_t row, uint64_t column) {
       return m_elements[column + row * m_columns];
     }
@@ -107,7 +72,7 @@ namespace QuEm {
       return result;
     }
   private:
-    T *m_elements = nullptr;
+    std::vector<T> m_elements;
     uint64_t m_rows;
     uint64_t m_columns;
   };
